18f6f89e3c6c8e970230ae016c64987164b00168
Snophysh <snophysh@gmail.com>
2020-12-05 22:51:11 -0600

    added improv and worked on ui and ink structure

--- electron-app/app/theCity.js
+++ electron-app/app/theCity.js
@@ -1,5 +1,28 @@
 const Story = require('inkjs').Story;
+const Improv = require('improv');
+const yaml = require('js-yaml');
 const fs = require("fs");
+
+/**
+ * Procedurally Generated Text
+ */
+
+// Load our data from a file
+const grammarData = yaml.load(fs.readFileSync(`${__dirname}/models/grammar.yaml`));
+
+// Create a generator object from this data
+const generator = new Improv(grammarData, {
+	filters: [Improv.filters.mismatchFilter()],
+	reincorporate: true
+});
+
+// Generate text and print it out
+console.log(generator.gen('root', {}));
+
+
+/**
+ *  Ink Story Setup and Code
+ */
 
 //load the ink file
 const inkFile = fs.readFileSync(`${__dirname}/theCityStory.json`, 'UTF-8').replace(/^\uFEFF/, ''); //strips the BOM
@@ -9,6 +32,13 @@ const story = new Story(inkFile);
 
 const buildings = ['Royal Smithy', 'The Leaning Tankard', 'Lion\'s Smithy', 'Grand Cathedral', 'The Academy', 'Healers House', 'Apothecary', 'Lord\'s Manor', 'Knights Guild', 'Lion\'s Tavern', 'Market Strip', 'Lion\'s Theater', 'Royal Gardens', 'Lion\'s Inn']
 const locations = ['Religious District', 'Industrial District', 'Market & Arts District', 'Town Center', 'Residential District', 'West Gate']
+
+//Observer Functions
+story.ObserveVariable("CurrentLocation", function (variableName, variableValue) {
+	let currentValue = variableValue.keys().next().value;
+	currentValue = JSON.parse(currentValue).itemName;
+	console.log(currentValue);
+});
 
 continueToNextChoice();
 
@@ -22,10 +52,20 @@ function continueToNextChoice() {
 	$('#buildings').empty()
 	$('#choices').empty()
 
+	
+
 	//write the story to the console until we find a choice
 	while (story.canContinue) {
 		let currentParagraphText = story.Continue()
-		$('#story-panel').append(`<p class="story-paragraphs">${currentParagraphText}</p>`)
+		let tags = story.currentTags;
+		processTags(tags);
+
+		if(tags.includes('description')) {
+			$('#location-description').append(`<p class="story-paragraphs">${currentParagraphText}</p>`)
+		} else {
+			$('#story-panel').append(`<p class="story-paragraphs">${currentParagraphText}</p>`)
+		}
+		
 	}
 
 	//check if there are choices
@@ -33,9 +73,9 @@ function continueToNextChoice() {
 		story.currentChoices.forEach((choice) => {
 			let choiceButton = `<button class="locations-and-buildings" id="choice-${choice.index}">${choice.text}</button>`;
 			//Check to see if the choice text is one of the locations or building and add them to the side bar
-			if(locations.includes(choice.text)) {
+			if (locations.includes(choice.text)) {
 				$('#locations').append(choiceButton)
-			} else if(buildings.includes(choice.text)) {
+			} else if (buildings.includes(choice.text)) {
 				$('#buildings').append(choiceButton)
 			} else {
 				$('#choices').append(`<button class="choice" id="choice-${choice.index}">${choice.text}</button>`)
@@ -57,5 +97,21 @@ function continueToNextChoice() {
 
 function end() {
 	console.log('THE END');
-	rl.close();
+}
+
+function processTags(tags) {
+	let keyPair = []
+	tags.forEach(tag => {
+		keyPair = tag.split(':');
+
+		if(keyPair.length === 2) {
+			switch (keyPair[0]) {
+				case 'LOCATION':
+				  $('#current-location-title').text(keyPair[1]);
+				  break;
+				default:
+				  console.log(`Sorry, ${keyPair[0]} was not found.`);
+			  }
+		}
+	})
 }
